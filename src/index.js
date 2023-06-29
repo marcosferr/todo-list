f9d8678a20bf487f7564d9889d70810ad4b4f939
marcosferr <ferreiramarcosadrian@gmail.com>
2023-06-29 00:08:54 -0400

    add header structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
+import "./styles/reset.css";
 import "./styles/styles.css";
 // import json from './data.json5';
-//on start up checked wheather its on light mode or dark mode
-const checkbox = document.getElementById("checkbox");
-if (checkbox.checked === true) {
-  document.body.classList.add("light");
-} else {
-  document.body.classList.remove("light");
-}
-
-//event listener for every time the dark mode toggle change
-checkbox.addEventListener("change", () => {
-  document.body.classList.toggle("light");
-});
